b6adbda92f57e3beab64b8fe6106d8d1ce5af202
OvandoFigueroa <0b3thovando@gmail.com>
2024-12-17 02:23:43 -0500

    DriverUpdate
    The driver has a prompt for the user to encode and decode instead of having a hardcoded result

--- Project3.cpp
+++ Project3.cpp
@@ -3,15 +3,56 @@
 
 int main()
 {
-	cout << "This program is running..." << endl;
+	//cout << "This program is running..." << endl;
 
-	string key = "Aumpkin";
+	string key;
+
+	cout << "Type in the word to use as the key: ";
+
+	cin >> key;
 
 	char * ptr =  &key[0];
 
-	Cipher testing(8, ptr);
+	Cipher testing(sizeof(key), ptr);
 
-	string input = "acbie";
-	cout << testing.decodeMessage(input) << endl;
+	string input;
+	
+	char option;
+
+	cout << "[e] encode with that key" << endl;
+
+	cout << "[d] decode with that key" << endl;
+
+	cout << "[q] quit" << endl;
+
+	cin >> option;
+
+	if (option == 'e')
+	{
+
+		cout << "Enter what to encode: ";
+
+		cin >> input;
+
+		cout << testing.encodeMessage(input) << endl;
+
+	}
+	else if (option == 'd')
+	{
+
+		cout << "Enter what to decode: ";
+
+		cin >> input;
+
+		cout << testing.decodeMessage(input) << endl;
+
+	}
+	else
+	{
+		return 0;
+	}
+
+
+
 
 }
